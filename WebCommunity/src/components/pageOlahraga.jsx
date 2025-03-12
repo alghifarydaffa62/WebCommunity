3f37196b5ac19f67bff570a3d51c0642f9b8c868
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-03-12 16:10:10 +0700

    tes

--- WebCommunity/src/components/pageOlahraga.jsx
+++ WebCommunity/src/components/pageOlahraga.jsx
@@ -5,7 +5,7 @@ export default function Olahraga() {
     return(
         <div>
             <Navbar/>
-            <h1 className='text-5xl text-center font-poppins text-[#174B86] font-bold'>KOMUNITAS OLAHRAGA</h1>
+            <h1 className='text-5xl text-center font-poppins text-[#174B86] font-bold'>Komunitas Olahraga</h1>
         </div>
     )
 }
