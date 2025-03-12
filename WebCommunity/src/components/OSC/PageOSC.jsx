e4769cd9fa0cb0a3e4e163e2af0ee422ecdb0a86
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-03-12 13:49:13 +0700

    osc page

--- WebCommunity/src/components/OSC/PageOSC.jsx
+++ WebCommunity/src/components/OSC/PageOSC.jsx
@@ -2,38 +2,59 @@ import "../../App.css"
 import Navbar from "../navbar"
 import { Link } from "react-router-dom"
 import osc from '../../assets/OSC (1).jpg'
-import WebdevDocum from "../Webdev/webdevDocum"
+import DocumOSC from "./DokumOSC"
+import Footer from "../footer"
 
 export default function OSC() {
     return(
         <div>
             <Navbar/>
 
-            <div className='flex justify-center gap-[4rem] items-center mt-8'>
-                <img src={osc} alt="" className='w-[25rem] h-[25rem]'/>
-                <div>
-                    <div className='mb-6'>
-                        <h1 className='text-4xl font-poppins text-[#174B86] font-bold'>Open Source Community</h1>
-                        <p className='mt-2 text-light font-poppins text-justify max-w-md'>Wadah bagi mahasiswa 
-                            yang tertarik dengan dunia open source dan keamanan siber. Di komunitas ini, 
-                            kamu bisa berdiskusi, berbagi ilmu, dan mengeksplorasi berbagai aspek teknologi, 
-                            termasuk cybersecurity dan CTF (Capture The Flag).</p>
-                    </div>
+            <div>
+                <div className="flex justify-center gap-16 items-center mt-18">
+                     <img src={osc} alt="Web Developer" className="w-[22rem] h-[22rem] object-contain" />
+            
                     <div>
-                        <h3 className='text-lg font-bold font-poppins text-[#174B86]'>Topik Pembahasan:</h3>
-                        <div className="flex gap-3 mt-2">
-                            <p className='text-sm py-2 px-4 bg-[#1A57A7] rounded-full font-poppins text-white'>CTF</p>
-                            <p className='text-sm py-2 px-4 bg-[#1A57A7] rounded-full font-poppins text-white'>Cyber Security</p>
-                            <p className='text-sm py-2 px-4 bg-[#1A57A7] rounded-full font-poppins text-white'>Linux</p>
-                            <p className='text-sm py-2 px-4 bg-[#1A57A7] rounded-full font-poppins text-white'>Open Source</p>
+                        <div className="mb-6 space-y-3">
+                            <h1 className="text-4xl font-poppins text-[#174B86] font-bold">
+                                Open Source Community
+                            </h1>
+                            <p className="text-justify text-gray-700 max-w-md">
+                            Wadah bagi mahasiswa yang tertarik dengan dunia open 
+                            source dan keamanan siber. Di komunitas ini, kamu bisa 
+                            berdiskusi, berbagi ilmu, dan mengeksplorasi berbagai 
+                            aspek teknologi, termasuk cybersecurity dan CTF (Capture The Flag).
+                            </p>
+                        </div>
+            
+                        <div className="space-y-3">
+                            <h3 className="text-lg font-bold text-[#174B86]">Topik Pembahasan:</h3>
+                            <div className="flex flex-wrap gap-3">
+                                <span className="text-sm py-2 px-4 bg-[#1A57A7] rounded-full text-white">
+                                    Cyber Security
+                                </span>
+                                <span className="text-sm py-2 px-4 bg-[#1A57A7] rounded-full text-white">
+                                    CTF
+                                </span>
+                                <span className="text-sm py-2 px-4 bg-[#1A57A7] rounded-full text-white">
+                                    Linux
+                                </span>
+                                <span className="text-sm py-2 px-4 bg-[#1A57A7] rounded-full text-white">
+                                    Open Source
+                                </span>
+                            </div>
                         </div>
                     </div>
-                    <button className='mt-8 py-3 px-5 bg-emerald-500 rounded-full font-poppins text-white font-semibold'>
-                        <Link to='/'>Gabung dengan komunitas</Link>
-                    </button>  
+                </div>
+
+                <div className="flex justify-center">
+                    <Link to="/" className="py-3 px-6 bg-emerald-500 rounded-full text-white font-semibold shadow-md hover:bg-emerald-600 transition">
+                        Gabung dngan komunitas
+                    </Link>
                 </div>
             </div>
-            <WebdevDocum/>
+            <DocumOSC/>
+            <Footer/>
         </div>
     )
 }
