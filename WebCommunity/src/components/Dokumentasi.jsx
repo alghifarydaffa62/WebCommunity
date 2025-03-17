675d249a4b7541707150d933fe1b60a97ae65d48
Navyz AZ <navyzaz17@gmai.com>
2025-03-17 10:56:41 +0700

    fixing error

--- WebCommunity/src/components/Dokumentasi.jsx
+++ WebCommunity/src/components/Dokumentasi.jsx
@@ -6,7 +6,7 @@ export default function Dokumentasi({dokum}) {
         <div className='bg-[#172036] py-12 font-poppins mt-16'>
             <h1 className='text-xl md:text-3xl font-bold text-white text-center mb-6'>Dokumentasi Kegiatan</h1>
             <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-                <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 place-items-center px-8 md:mx-40 lg:mx-52 2xl:mx-72'>
+                <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 place-items-center px-8 md:mx-40 2xl:mx-72'>
                     {dokum.map((imgSrc, index) => (
                         <img key={index} src={imgSrc} alt={`Dokumentasi ${index + 1}`} 
                             className='w-full md:w-[30rem] h-[20rem] object-cover rounded-lg'/>
